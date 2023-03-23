3f25f1941f982a00fb96f1ab44cd9e646ac05abf
Leticia Takenaka <leticiatakenaka12@gmail.com>
2023-03-23 20:18:40 -0300

    cards layout

---
+++ src/components/Cards.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import styled from "styled-components";
+import api from "../service/api";
+
+const CardStyle = styled.div`
+  background: #efefef;
+  height: 15em;
+  width: 11em;
+
+  @media screen and (max-width: 1023px) {
+  }
+`;
+const CardTextStyle = styled.div`
+  @media screen and (max-width: 1023px) {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+function Cards() {
+  const [comicsList, setComicsList] = useState([]);
+
+  useEffect(() => {
+    api
+      .get(
+        "/v1/public/comics?ts=1&apikey=8fed82c36a49172b887ffe0bc9cf8478&hash=11f5fb667b1fa98d74ae4798ee82031e&limit=12"
+      )
+      .then((response) => {
+        setComicsList([...response.data.data.results]);
+      })
+      .catch((err) => {
+        console.error("ops! ocorreu um erro" + err);
+      });
+  }, []);
+
+  return (
+    <>
+      {comicsList.map((comic) => (
+        <div key={comic.id} className="item">
+          <CardStyle />
+          <CardTextStyle>
+            <h5>{comic.title}</h5>
+          </CardTextStyle>
+        </div>
+      ))}
+    </>
+  );
+}
+
+export default Cards;
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,38 +1,29 @@
 import styled from "styled-components";
-import Card from "./Card";
-import api from "../service/api";
-import { useEffect } from "react";
+import Cards from "./Cards";
 
 const MainStyle = styled.div`
-  height: 400px;
+  height: 9em;
+  gap: 20px;
+  display: grid;
 
   @media screen and (max-width: 1023px) {
-    margin-top: 80px;
-    display: flex;
+    margin-top: 2em;
     justify-content: center;
+    grid-template-columns: 11em 11em;
   }
+
   @media screen and (min-width: 1024px) {
-    height: 400px;
     margin-left: 20em;
-    margin-right: 18em;
-    margin-top: 10em;
+    margin-right: 20em;
+    margin-top: 7em;
+    grid-template-columns: 200px 200px 200px 200px 200px;
   }
 `;
 
 function Main() {
-  useEffect(() => {
-    api
-      .get(
-        "/v1/public/stories?ts=1&apikey=8fed82c36a49172b887ffe0bc9cf8478&hash=11f5fb667b1fa98d74ae4798ee82031e"
-      )
-      .then((response) => console.log(response.data))
-      .catch((err) => {
-        console.error("ops! ocorreu um erro" + err);
-      });
-  }, []);
   return (
     <MainStyle>
-      <Card />
+      <Cards />
     </MainStyle>
   );
 }
